71fa8abcb54a5b6c5437809120e248ad7ef43f2a
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-27 19:58:48 -0300

    Test Planet - getAll

--- src/test/java/yasmingv/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/yasmingv/sw_planet_api/domain/PlanetServiceTest.java
@@ -7,16 +7,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static yasmingv.sw_planet_api.common.PlanetConstants.PLANET;
 import static yasmingv.sw_planet_api.common.PlanetConstants.INVALID_PLANET;
 
-import yasmingv.sw_planet_api.domain.entity.Planet;
-import yasmingv.sw_planet_api.domain.respository.PlanetRepository;
-import yasmingv.sw_planet_api.domain.service.PlanetService;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -82,5 +83,31 @@ public class PlanetServiceTest {
         Optional<Planet> sut = planetService.getByName(name);
 
         assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets() {
+        List<Planet> planets = new ArrayList<>() {
+            {
+            add(PLANET);
+            }
+        };
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
+        when(planetRepository.findAll(query)).thenReturn(planets);
+
+        List<Planet> sut = planetService.getAll(PLANET.getTerrain(), PLANET.getClimate());
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut).hasSize(1);
+        assertThat(sut.get(0)).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void listPlanets_ReturnsNoPlanets(){
+        when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
+
+        List<Planet> sun = planetService.getAll(PLANET.getClimate(), PLANET.getTerrain());
+
+        assertThat(sun).isEmpty();
     }
 }
