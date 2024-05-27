5ba13b40eefd62a1bd9648cfc0ef221b99341d1a
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-27 17:25:51 -0300

    Test Planet - getById

--- src/test/java/yasmingv/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/yasmingv/sw_planet_api/domain/PlanetServiceTest.java
@@ -1,18 +1,23 @@
 package yasmingv.sw_planet_api.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static yasmingv.sw_planet_api.common.PlanetConstants.PLANET;
+import static yasmingv.sw_planet_api.common.PlanetConstants.INVALID_PLANET;
 
 import yasmingv.sw_planet_api.domain.entity.Planet;
 import yasmingv.sw_planet_api.domain.respository.PlanetRepository;
 import yasmingv.sw_planet_api.domain.service.PlanetService;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
@@ -30,5 +35,31 @@ public class PlanetServiceTest {
        Planet sut = planetService.create(PLANET);
 
        assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidDate_ThrowsException(){
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.getById(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty() {
+        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.getById(1L);
+
+        assertThat(sut).isEmpty();
     }
 }
