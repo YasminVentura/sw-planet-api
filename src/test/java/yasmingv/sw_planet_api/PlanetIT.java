6ca36e2afb67a53546a3991af8a782141bd2085c
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-30 19:18:52 -0300

    PlanetIT - test getId planet

--- src/test/java/yasmingv/sw_planet_api/PlanetIT.java
+++ src/test/java/yasmingv/sw_planet_api/PlanetIT.java
@@ -13,9 +13,11 @@ import yasmingv.sw_planet_api.domain.Planet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static yasmingv.sw_planet_api.common.PlanetConstants.PLANET;
+import static yasmingv.sw_planet_api.common.PlanetConstants.TATOOINE;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = { "/import_planets.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = { "/remove_planets.sql" }, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
 
@@ -31,5 +33,13 @@ public class PlanetIT {
         assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
         assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
         assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void getPlanet_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
     }
 }
