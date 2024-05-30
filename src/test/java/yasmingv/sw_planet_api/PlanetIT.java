91afcb62bfa5a66129b53e207bf1830dfdf83d50
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-30 19:02:15 -0300

    PlanetIT - test create planet

---
+++ src/test/java/yasmingv/sw_planet_api/PlanetIT.java
@@ -0,0 +1,35 @@
+package yasmingv.sw_planet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import yasmingv.sw_planet_api.domain.Planet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static yasmingv.sw_planet_api.common.PlanetConstants.PLANET;
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = { "/remove_planets.sql" }, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+public class PlanetIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}
